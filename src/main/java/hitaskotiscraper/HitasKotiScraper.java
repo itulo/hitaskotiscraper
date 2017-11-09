e81922eb7ea9507c1474c0b7d885123f47267f3d
Italo Armenti <italo.armenti@gmail.com>
2017-11-09 23:17:52 +0200

    scrape address of first advertisement

--- src/main/java/hitaskotiscraper/HitasKotiScraper.java
+++ src/main/java/hitaskotiscraper/HitasKotiScraper.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/**
+ * 
+ * @author italoarmenti
+ *
+ */
 public class HitasKotiScraper {
 	
 	private final static String URL = "http://www.hitaskoti.fi/haku_hakutulokset";
@@ -19,7 +25,23 @@ public class HitasKotiScraper {
 	}
 	
 	static void scrape() throws IOException{
-		Document doc = Jsoup.connect(URL).get();
-		String title = doc.title();
+		Document page = Jsoup.connect(URL).get();
+		Element firstAd = page.selectFirst("tr.taulu_vari1");
+		String date = page.selectFirst("td.borderright").text();
+		
+		System.out.println(firstAd.toString());
+		
+		System.out.println(date);
+		
+		Elements elementA = firstAd.select("a");
+		System.out.println(elementA.toString());
+		
+		String homeUrl = elementA.attr("href");
+		System.out.println(homeUrl);
+		
+		String address = elementA.select("strong").text();
+		System.out.println(address);
+		
+		String zip = elementA.select("span").text().replace(" ","");
 	}
 }
---
+++ src/test/java/hitaskotiscraper/HitasKotiScrapeTest.java
@@ -0,0 +1,5 @@
+package hitaskotiscraper;
+
+public class HitasKotiScrapeTest {
+	
+}
